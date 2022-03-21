e12eb8604cffc2892fd85ac45c4785ceff4a071f
JFJ1972 <97111500+JFJ1972@users.noreply.github.com>
2022-03-21 05:43:50 -0500

    comentarios a scrolling

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./Navbar.css";
 
-const Navbar = ({ isScrolling }) => {
-    const toTheTop = () => {
-        window.scrollTo({top:0, left:0, behavior: "smooth"});
-    };
+const Navbar = ({ isScrolling }) => {//se trae por desestructuracion de appjs para no hacer props
+  
+    const toTheTop = () => { // esta funcion nos recgresa al inicio click en logo
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
 
   return (
-    <nav className={`navbar ${ isScrolling > 20 ? "scrolling" : null}`}>
-    <div className="navbar-logo" onClick={toTheTop}>Jose F. Jimenez</div>
+    <nav className={`navbar ${isScrolling > 20 ? "scrolling" : null}`}>
+      <div className="navbar-logo" onClick={toTheTop}>
+        Jose F. Jimenez
+      </div>
     </nav>
   );
-  
 };
 
-export default Navbar
+export default Navbar;
